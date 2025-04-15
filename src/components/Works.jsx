374b800655958f6c6c60ec3b8ee469176424a467
sam209976 <hawkc711@gmail.com>
2025-04-15 17:02:49 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,3 +1,11 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { projects } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
 const ProjectCard = ({ name, description, tags }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", 0.5, 0.75)}>
@@ -18,3 +26,28 @@ const ProjectCard = ({ name, description, tags }) => {
     </motion.div>
   );
 };
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+
+      <div className='w-full flex'>
+        <p className='mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+          Here are some of the projects I've worked on recently that reflect my experience and skillset.
+        </p>
+      </div>
+
+      <div className='mt-20 flex flex-wrap gap-7'>
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "work");
