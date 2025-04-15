aacde406cf8746833ce7c3ba736709e5958dc30d
sam209976 <hawkc711@gmail.com>
2025-04-15 16:13:01 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaInstagram, FaLinkedin } from "react-icons/fa6";
+
+const Footer = () => {
+  return (
+    <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
+      <a
+        href="https://www.instagram.com/__sam_eer____"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram size={24} />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/sameer-ahmad-a1908828a"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin size={24} />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
