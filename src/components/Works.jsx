46f3002af79eb17873c6dd4367216dd73ee1869f
sam209976 <hawkc711@gmail.com>
2025-04-15 17:00:25 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,7 @@
-const ProjectCard = ({ name, description, tags, source_code_link }) => {
+const ProjectCard = ({ name, description, tags }) => {
   return (
     <motion.div variants={fadeIn("up", "spring", 0.5, 0.75)}>
       <div className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'>
-        {/* Image removed */}
-        
         <div className='mt-5'>
           <h3 className='text-white font-bold text-[24px]'>{name}</h3>
           <p className='mt-2 text-secondary text-[14px]'>{description}</p>
@@ -15,17 +13,6 @@ const ProjectCard = ({ name, description, tags, source_code_link }) => {
               #{tag.name}
             </p>
           ))}
-        </div>
-
-        <div className='mt-4 flex items-center justify-end'>
-          <a
-            href={source_code_link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-white hover:text-gray-400"
-          >
-            <GitHubIcon />
-          </a>
         </div>
       </div>
     </motion.div>
